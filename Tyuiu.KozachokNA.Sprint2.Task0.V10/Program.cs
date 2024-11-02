8c01e4da780b32a0a7e79c891e5db58fb855109f
PericlesDarth <aanonimov210@gmail.com>
2024-11-02 09:09:34 +0500

    /

---
+++ Tyuiu.KozachokNA.Sprint2.Task0.V10/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KozachokNA.Sprint2.Task0.V10.Lib;
+
+namespace Tyuiu.KozachokNA.Sprint2.Task0.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            DataService dataService = new DataService();
+
+            bool[] foo = dataService.GetCompareOperations(a, b);
+            Console.Write(foo);
+
+            Console.ReadKey();
+        }
+    }
+}
