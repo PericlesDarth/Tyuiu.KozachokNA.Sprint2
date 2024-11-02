2786c249f05021f6186e312d261c23e9738251d6
PericlesDarth <aanonimov210@gmail.com>
2024-11-02 08:46:24 +0500

    /

---
+++ Tyuiu.KozachokNA.Sprint2.Task0.Lib/Class1.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.KozachokNA.Sprint2.Task0.Lib
+{
+    public class Class1: ISprint2Task0V10
+    {
+        public bool[] GetCompareOperations(int a, int b)
+        {
+            bool[] result = new bool[6];
+            result[0] = a == b;
+            result[1] = a != b;
+            result[2] = a < b;
+            result[3] = a > b;
+            result[4] = a <= b;
+            result[5] = a >= b;
+            return result;
+        }
+    }
+}
